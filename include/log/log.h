22045b9c5d9eeb80c3dd65d288f4f90021210f31
Frasier <fsundra@gmail.com>
2026-08-28 22:05:27 +0800

    [main] adds log_errno a log for printing the errno message to the screen if a function sets it

--- include/log/log.h
+++ include/log/log.h
@@ -4,5 +4,6 @@
 typedef enum { LOG_DEBUG, LOG_INFO, LOG_WARN, LOG_ERROR } log_level;
 
 void log_message(log_level level, const char *format, ...);
+void log_errno(log_level level, const char *context);
 
 #endif // !LOG_H
--- src/log/log.c
+++ src/log/log.c
@@ -1,6 +1,8 @@
 #include "log/log.h"
+#include <errno.h>
 #include <stdarg.h>
 #include <stdio.h>
+#include <string.h>
 
 void log_message(log_level level, const char *format, ...) {
     va_list args;
@@ -26,6 +28,13 @@ void log_message(log_level level, const char *format, ...) {
     va_start(args, format);
     vfprintf(stderr, format, args);
     va_end(args);
+    fputc('\n', stderr);
 
     return;
+}
+
+void log_errno(log_level level, const char *context) {
+    int error = errno;
+
+    log_message(level, "%s: %s", context, strerror(error));
 }
