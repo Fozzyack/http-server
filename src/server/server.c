d3417f58b242a7d2103a5cfbbf731892caa90c94
Frasier <fsundra@gmail.com>
2026-08-28 21:41:13 +0800

    [main] uses log_message for debugging

--- src/server/server.c
+++ src/server/server.c
@@ -1,4 +1,5 @@
 #include "server/server.h"
+#include "log/log.h"
 #include <arpa/inet.h>
 #include <asm-generic/socket.h>
 #include <netinet/in.h>
@@ -11,7 +12,7 @@
 tcp_server_status init_server(tcp_server_info *server_info, int port) {
 
     if (!server_info) {
-        fprintf(stderr, "server_info struct does not exist\n");
+        log_message(LOG_ERROR, "server_info is NULL");
         return SERVER_INFO_DOES_NOT_EXIST;
     }
 
@@ -32,6 +33,7 @@ tcp_server_status bind_tcp_server(tcp_server_info *server_info, int port) {
     int fd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
     if (fd == -1) {
         perror("socket");
+        log_message(LOG_ERROR, "socket error");
         return SERVER_SOCKET_INIT_ERROR;
     }
     server_info->socket_fd = fd;
@@ -40,20 +42,24 @@ tcp_server_status bind_tcp_server(tcp_server_info *server_info, int port) {
     int reuse_port = 1;
     if (setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &reuse_addr, sizeof(reuse_addr)) == -1) {
         perror("setsockopt");
+        log_message(LOG_ERROR, "setsockopt error");
         return SERVER_SETSOCKOPT_ERROR;
     }
     if (setsockopt(fd, SOL_SOCKET, SO_REUSEPORT, &reuse_port, sizeof(reuse_port)) == -1) {
         perror("setsockopt");
+        log_message(LOG_ERROR, "setsockopt error");
         return SERVER_SETSOCKOPT_ERROR;
     }
 
     if (bind(fd, (struct sockaddr *)server_info, sizeof(*server_info)) == -1) {
         perror("bind");
+        log_message(LOG_ERROR, "bind error");
         return SERVER_BIND_ERROR;
     }
 
     if (listen(fd, LISTEN_BACKLOG) == -1) {
         perror("bind");
+        log_message(LOG_ERROR, "listen error");
         return SERVER_BIND_ERROR;
     }
 
@@ -67,6 +73,7 @@ tcp_server_status accept_client(int server_fd, int *client_fd) {
     int fd = accept(server_fd, (struct sockaddr *)&client_info, &client_info_len);
     if (fd == -1) {
         perror("accept");
+        log_message(LOG_ERROR, "accept error");
         return SERVER_ACCEPT_ERROR;
     }
 
