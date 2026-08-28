4ec9f51ab6e086068df811e9d146c6556d085a08
Frasier <fsundra@gmail.com>
2026-08-28 23:15:12 +0800

    [main] smoke testing parser

--- src/main.c
+++ src/main.c
@@ -1,7 +1,22 @@
+#include "http/parser.h"
 #include "log/log.h"
 #include "server/server.h"
 #include <stdlib.h>
 #include <unistd.h>
+
+void handle_client(int client_fd) {
+    http_request req = {0};
+
+    if (read_http_request(client_fd, &req) != PARSE_READ_OK) {
+        log_message(LOG_ERROR, "Failed to parse HTTP request");
+        close(client_fd);
+        return;
+    }
+    log_message(LOG_INFO, "HTTP request parsed");
+    log_message(LOG_INFO, "method: %s", req.method);
+    log_message(LOG_INFO, "path: %s", req.path);
+    log_message(LOG_INFO, "protocol: %s", req.protocol);
+}
 
 int main(void) {
 
@@ -19,7 +34,9 @@ int main(void) {
         return EXIT_FAILURE;
     }
 
+    handle_client(client_fd);
     close(client_fd);
+
     close(server_info.socket_fd);
 
     return EXIT_SUCCESS;
