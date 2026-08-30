291c14b2850269b47b01dd1782ad3073b1d1ae01
Frasier <fsundra@gmail.com>
2026-08-30 15:35:58 +0800

    adds http response struct

--- include/http/response.h
+++ include/http/response.h
@@ -1,7 +1,20 @@
 #ifndef RESPONSE_H
 #define RESPONSE_H
 
+#include "http/parser.h"
+#include <stddef.h>
+#define STATUS_RESPOSNE_LENGTH 64
+
 typedef struct http_response {
+
+    int status;
+    int status_response[STATUS_RESPOSNE_LENGTH];
+
+    http_header *headers;
+    size_t header_count;
+
+    char *buffer;
+    size_t buffer_size;
 
 } http_response;
 
