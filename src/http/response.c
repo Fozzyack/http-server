8ed48f614695806ec1f516b8bf572a723a70fd91
Frasier <fsundra@gmail.com>
2026-09-01 18:56:19 +0800

    [main] sends back response

--- src/http/response.c
+++ src/http/response.c
@@ -5,11 +5,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/socket.h>
 
 void init_response(http_response *response) {
     printf("init res structure\n");
     response->status = 200;
-    memset(response->status_response, '\0', STATUS_RESPOSNE_LENGTH);
+    memset(response->status_response, 0, STATUS_RESPOSNE_LENGTH);
+    response->status_response[0] = 'O';
+    response->status_response[1] = 'K';
     response->headers = NULL;
     response->header_count = 0;
     response->body = NULL;
@@ -69,4 +72,59 @@ http_response_status response_set_json(const char *json_string, http_response *r
     response->body[length + 1] = '\0';
 
     return RESPONSE_OK;
+}
+
+char *construct_response(const http_response *response, size_t *response_length) {
+    size_t buffer_size = 1024;
+    char *buffer = malloc(buffer_size);
+    if (!buffer) {
+        log_errno(LOG_ERROR, "Failed to malloc for buffer");
+        return NULL;
+    }
+
+    size_t offset = snprintf(buffer, buffer_size, "HTTP/1.1 %d %s\r\n", response->status, response->status_response);
+    for (size_t i = 0; i < response->header_count; i++) {
+        size_t header_length = snprintf(NULL, "%s: %s\r\n", response->headers[i].name, response->headers[i].value);
+        while (offset + header_length + 1 > buffer_size) {
+            buffer_size *= 2;
+            buffer = realloc(buffer, buffer_size);
+            if (!buffer) {
+                log_errno(LOG_ERROR, "Failed to malloc for buffer");
+                return NULL;
+            }
+        }
+        offset += snprintf(buffer + offset, buffer_size - offset, "%s: %s\r\n", response->headers[i].name,
+                           response->headers[i].value);
+    }
+    offset += snprintf(buffer + offset, buffer_size - offset, "\r\n");
+    if (response->body) {
+        while (offset + response->body_size > buffer_size) {
+            buffer_size *= 2;
+            buffer = realloc(buffer, buffer_size);
+            if (!buffer) {
+                log_errno(LOG_ERROR, "Failed to malloc for buffer");
+                return NULL;
+            }
+            memcpy(buffer + offset, response->body, response->body_size);
+            offset += response->body_size;
+        }
+    }
+    *response_length = offset;
+    return buffer;
+}
+
+http_response_status send_response(int client_fd, http_response *response) {
+    size_t buffer_size = 0;
+    char *response_data = construct_response(response, &buffer_size);
+
+    size_t bytes_sent = 0;
+    while (bytes_sent < buffer_size) {
+        ssize_t b_sent = send(client_fd, response_data + bytes_sent, buffer_size - bytes_sent, 0);
+        if (bytes_sent <= 0) {
+            log_errno(LOG_ERROR, "Failed to send Response");
+            break;
+        }
+
+        bytes_sent += (size_t)b_sent;
+    }
 }
