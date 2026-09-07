4ef057defcd8fed8286780ca4372fe672b0543c1
Frasier <fsundra@gmail.com>
2026-09-07 16:38:42 +0800

    [router-setup] adds router init function

--- src/routes/router.c
+++ src/routes/router.c
@@ -1,5 +1,11 @@
 #include "routes/router.h"
 
+router_status init_route(router *r) {
+    r->routes = NULL;
+    r->route_count = 0;
+    return ROUTER_OK;
+}
+
 router_status add_route(char *path, (*handler)(void *args)) { return ROUTER_OK; }
 
 router_status setup_routes(void) { return ROUTER_OK; }
