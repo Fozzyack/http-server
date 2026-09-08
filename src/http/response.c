9f80fef3668fff966bebbf1dfe0655dc09cfb8bc
Frasier <fsundra@gmail.com>
2026-09-08 22:33:10 +0800

    prepares response rewrite

--- src/http/response.c
+++ src/http/response.c
@@ -1,5 +1,8 @@
 #include "http/http.h"
 #include "log/log.h"
+#include <asm-generic/errno-base.h>
+#include <asm-generic/errno.h>
+#include <errno.h>
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -118,9 +121,11 @@ http_response_status send_response(int client_fd, http_response *response) {
     size_t bytes_sent = 0;
     while (bytes_sent < buffer_size) {
         ssize_t b_sent = send(client_fd, response_data + bytes_sent, buffer_size - bytes_sent, 0);
-        if (b_sent <= 0) {
-            log_errno(LOG_ERROR, "Failed to send Response");
-            break;
+        if (b_sent == -1) {
+            int err = errno;
+            if (err == EAGAIN || err == EWOULDBLOCK) {
+                return RESPONSE_OK;
+            }
         }
 
         bytes_sent += (size_t)b_sent;
